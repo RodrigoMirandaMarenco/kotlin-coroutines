5b70a81931c44f9df0968e714cb85f63d395de48
Rodrigo Miranda <rodrigo.kreloco@gmail.com>
2020-10-20 16:49:50 -0600

    :sparkles: Add flow functions to PlantRepository

--- advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
+++ advanced-coroutines-codelab/start/src/main/java/com/example/android/advancedcoroutines/PlantRepository.kt
@@ -25,6 +25,7 @@ import com.example.android.advancedcoroutines.util.CacheOnSuccess
 import com.example.android.advancedcoroutines.utils.ComparablePair
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.withContext
 
 /**
@@ -65,6 +66,21 @@ class PlantRepository private constructor(
                 emit(plantList.applyMainSafeSort(customSortOrder))
             }
         }
+
+    /**
+     * Fetch a list of [Plant]s from the database.
+     * Returns a Flow List of Plants.
+     */
+    val plantsFlow: Flow<List<Plant>>
+        get() = plantDao.getPlantsFlow()
+
+    /**
+     * Fetch a list of [Plant]s from the database that matches a given [GrowZone].
+     * Returns a Flow List of Plants.
+     */
+    fun getPlantsWithGrowZoneFlow(growZone: GrowZone): Flow<List<Plant>> {
+        return plantDao.getPlantsWithGrowZoneNumberFlow(growZone.number)
+    }
 
     /**
      * Returns true if we should make a network request.
